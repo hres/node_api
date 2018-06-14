60e6af7c275ff3fd5a203fb6f6e2f37b348b974a
Bimal Bhagrath <30774646+bimo16@users.noreply.github.com>
2018-06-14 15:30:30 -0400

    Add files via upload

--- Documentation/main.js
+++ Documentation/main.js
@@ -4,11 +4,9 @@ function getAPIKey() {
   var email = $("#get-key-email").val();
   console.log(email);
 
-  $("#new-key-div").prop("hidden", false);
-
   $.get("https://node.hres.ca/key?email=" + email, (res) => {
 
-    $("#new-api-key").html(res.key);
+    $("#new-key-div").html("New API Key: <code>" + res.key + "</code>");
   })
     .fail((error) => {
       $("#new-api-key").html("API key error");
@@ -20,5 +18,5 @@ function retrieveAPIKey() {
   var email = $("#retrieve-key-email").val();
   console.log(email);
 
-  $("#retrieve-key-div").prop("hidden", false);
+  $("#retrieve-key-div").html("Your API Key has been emailed to you.");
 }
