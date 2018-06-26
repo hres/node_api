60be47bdb9c5a14a76a38b6f555458637960c7f8
Bimal Bhagrath <30774646+bimo16@users.noreply.github.com>
2018-06-26 11:53:53 -0400

    Add files via upload

--- Node/index.js
+++ Node/index.js
@@ -84,7 +84,7 @@ api.get('/statistics', (req, res) => {
   });
 });
 
-api.get('/qmod', (req, res) {
+api.get('/qmod', (req, res) => {
 
   // restrict which fields can be aggregated on and list them to return
 
@@ -94,8 +94,7 @@ api.get('/qmod', (req, res) {
     };
   });
 
-  return indices;
-
+  return res.status(200).json(indices);
 });
 
 function includeElasticResult(esres) {
