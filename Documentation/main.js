8911c0f0f06c65f7110980e93059028c56d78697
Bimal Bhagrath <30774646+bimo16@users.noreply.github.com>
2018-06-26 12:14:37 -0400

    Add files via upload

--- Documentation/main.js
+++ Documentation/main.js
@@ -1,5 +1,49 @@
 
+let histogramOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+};
+let termsOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+};
+
+var ctx;
+
 $(document).ready(() => {
+
+  $.get("http://node.hres.ca/_info", (res) => {
+
+    res.indices.forEach((index) => {
+
+      var path = index.split("/");
+
+      switch (path[1]) {
+        case "drug":
+          $("#drug_ends").append("<option value='" + index + "'>" + index + "</option>");
+          break;
+        case "food":
+          $("#food_ends").append("<option value='" + index + "'>" + index + "</option>");
+          break;
+        case "device":
+          $("#device_ends").append("<option value='" + index + "'>" + index + "</option>");
+          break;
+        default:
+          $("#other_ends").append("<option value='" + index + "'>" + index + "</option>");
+          break;
+      }
+    });
+  });
 
   // resources not loading allow timeout... fix this
   setTimeout(() => {
@@ -37,7 +81,7 @@ function retrieveAPIKey() {
 
 function buildQuery() {
 
-  var query = "https://node.hres.ca/";
+  var query = "https://node.hres.ca";
   query += $("#query-endpoint").val();
 
   var params = {};
@@ -70,8 +114,11 @@ function buildQuery() {
 
   $.get(query, (res) => {
 
+    $("#ctx-open").prop("hidden", true);
     $("#json-response").val(JSON.stringify(res, null, 2));
-    setChart(res);
+    if (res.hasOwnProperty("plot")) {
+      setChart(res);
+    }
   })
     .fail((err) => {
 
@@ -79,4 +126,68 @@ function buildQuery() {
     });
 }
 
-function setChart(rawData) {}
+function setChart(rawData) {
+
+  if (ctx) {
+    ctx.destroy();
+  }
+
+  $("#ctx-open").prop("hidden", false);
+
+  let ctxDiv = document.getElementById('data-chart');
+  let data = rawData.plot == "histogram" ? buildHistogramData(rawData.results) : buildTermsData(rawData.results);
+
+  ctx = new Chart(ctxDiv, {
+    type: rawData.plot == "histogram" ? "line" : "bar",
+    data: data,
+    options: rawData.plot == "histogram" ? histogramOptions : termsOptions
+  });
+}
+
+function buildHistogramData(results) {
+
+  let labels = results.map((result) => {
+    return result.key_as_string
+  });
+
+  let datasets = [
+    {
+      label: "Health Canada",
+      data: results.map((result) => {
+        return result.doc_count
+      }),
+      borderColor: "#d81200",
+      backgroundColor: "rgba(216, 18, 0, 0.4)",
+      pointRadius: 5
+    }
+  ];
+
+  return {
+    labels: labels,
+    datasets: datasets
+  };
+}
+
+function buildTermsData(results) {
+
+  let labels = results.map((result) => {
+    return result.key
+  });
+
+  let datasets = [
+    {
+      label: "Health Canada",
+      data: results.map((result) => {
+        return result.doc_count
+      }),
+      borderWidth: 1,
+      borderColor: "#d81200",
+      backgroundColor: "rgba(216, 18, 0, 0.4)",
+    }
+  ];
+
+  return {
+    labels: labels,
+    datasets: datasets
+  };
+}
