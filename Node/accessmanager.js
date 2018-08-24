bff75e0864b8a0beec5b00d5c5e6165e8ebb3de1
Bimal Bhagrath <30774646+bimo16@users.noreply.github.com>
2018-08-24 15:15:28 -0400

    Add files via upload

--- Node/accessmanager.js
+++ Node/accessmanager.js
@@ -17,6 +17,7 @@ exports.newAccount = async (email, password) => {
 
   const salt = crypto.randomBytes(8).toString('hex');
   const pass = crypto.createHash('sha256').update(password + salt).digest('hex');
+  const key = crypto.randomBytes(8).toString('hex');
 
   const time = new Date();
 
@@ -27,29 +28,15 @@ exports.newAccount = async (email, password) => {
   const insertUserQuery = "INSERT INTO users(user_email, salt, password, sign_up_date) VALUES($1, $2, $3, $4)";
   const insertUserValues = [email, salt, pass, pgDate];
   const insertKeyQuery = "INSERT INTO api_keys(user_email, key) VALUES($1, $2)";
+  const insertKeyValues = [email, key];
 
   try {
     await pool.connect();
     var users = await pool.query(userQuery, userValues);
 
     if (users.rows.length < 1) {
-        var success = false;
-        var key;
-
         var insertUser = await pool.query(insertUserQuery, insertUserValues);
-
-        while (!success) {
-          key = crypto.randomBytes(8).toString('hex');
-          const insertKeyValues = [email, key];
-
-          try {
-             var insertKey = await pool.query(insertKeyQuery, insertKeyValues);
-             success = true;
-          }
-          catch (err) {
-            success = false;
-          }
-        }
+        var insertKey = await pool.query(insertKeyQuery, insertKeyValues);
 
         await pool.end();
         return {
